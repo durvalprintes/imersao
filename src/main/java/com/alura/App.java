e71971edf1a897bf3a9e77c08b4f31227364ae29
Durval Printes <durvalprintes@gmail.com>
2022-07-18 15:46:17 -0300

    Projeto Maven inicial

---
+++ src/main/java/com/alura/App.java
@@ -0,0 +1,13 @@
+package com.alura;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
